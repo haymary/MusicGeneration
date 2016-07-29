fd710f29d7fbf78613b97b60cbf0c80990584c83
Jmuksin <Muksinova.j.a@gmail.com>
2016-07-30 01:34:18 +0300

    рабочий парсер

--- src/Parser/ChordParser.java
+++ src/Parser/ChordParser.java
@@ -6,7 +6,7 @@ import java.util.Random;
 import Gene.Chord;
 import Genome.AbstractInstrument;
 import Genome.PianoGenome;
-import Genome.ViolinGenome;
+
 
 public class ChordParser extends GenomeParser {
 
@@ -26,15 +26,23 @@ public class ChordParser extends GenomeParser {
         int duration = 0;
         String result = new String();
         result = CheckType(individual);
-        for (int i = 1; i < notes.size(); i++) {
+        int k = 0;
+        for (int i = 0; i < notes.size(); i++) {
+            System.out.println(notes.get(i).getValue());
+            System.out.println(notes.get(i).is_continues_last());
+
             if (notes.get(i).is_continues_last()) {
                 duration++;
             } else {
-                result += " " + NumberToNoteOrChord(notes.get(i - 1).getValue(), duration, notes.get(i - 1).getOctave_num());
+                result += " " + NumberToNoteOrChord(notes.get(k).getValue(), duration, notes.get(k).getOctave_num());
+               if(k==i) k=i+1;
+                else k=i;
                 duration = 0;
             }
+            System.out.println(result);
 
         }
+        result += " " + NumberToNoteOrChord(notes.get(k).getValue(), duration, notes.get(k).getOctave_num());
 
         return result;
     }
@@ -49,38 +57,43 @@ public class ChordParser extends GenomeParser {
         } else if (number == 0) {
             result = "R" + NumberToDuration(duration);
         } else if (number < 0 && number > -29) {
-            result = not[(-1 * number - 1) % 7]+ Integer.toString(octave) + chord[(-1 * number - 1) / 7] + NumberToDuration(duration).toUpperCase();
+            int c=number%7==0?6:(-1*number)%7-1;
+            result = not[c] + Integer.toString(octave) + chord[(-1 * number - 1) / 7] + NumberToDuration(duration).toUpperCase();
         }
         return result;
 
     }
+
     //Method for translating numbers of sixteenths to duration
     private String NumberToDuration(final int n) {
         String result = new String();
         if (n >= 16)
             for (int i = 0; i < n / 16; i++)
                 result += "w";
-
         String[] duration = {"s", "i", "i.", "q", "qs", "qi", "qi.", "h", "hs", "hi", "hi.", "hq", "hqs", "hqi", "hqi.", "w"};
-        if (n % 16 != 0)
-            result += duration[n % 16 - 1];
-
+        if (n % 16 != 0 || n == 0)
+            result += duration[n % 16];
         return result;
 
     }
 
     private String CheckType(final AbstractInstrument individual) {
         String result = new String();
-
         Random rand = new Random(); //Random choose of instrument in JFugue
         if (individual.getInstrumentType() == "Piano") {
-
             result = "I" + Integer.toString(rand.nextInt(7));
         } else if (individual.getInstrumentType() == "Violin") {
-
             result = "I" + Integer.toString(rand.nextInt(7) + 40);
         }
         return result;
     }
 
+    public static void main(String[] args) {
+        PianoGenome p = new PianoGenome();
+        ChordParser ch = new ChordParser();
+
+        System.out.println(ch.translateToPhenotype(p.generateIndividual()));
+    }
+
 }
+
